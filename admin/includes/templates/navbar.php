ff886ad0335d04b88668f5330510821fc26af01c
mhslekbar <mhs.lekbar@gmail.com>
2022-03-06 14:03:33 +0000

    Create navbar.php

---
+++ admin/includes/templates/navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container">
+    <a class="navbar-brand" href="DashBoard.php">Table De Board</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+      <div class="dropdown">
+          <a class="nav-link a-nav dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <?=$_SESSION['username']??null?>
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="users.php?do=changepass&userid=<?=$_SESSION['userid']?>">Changer mot de passe</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="logout.php">Deconnecter</a></li>
+          </ul>
+      </div>
+    </div>
+  </div>
+</nav>
