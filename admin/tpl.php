c063c9d73b4b21ed36d497c2177aa67108b95872
mhslekbar <mhs.lekbar@gmail.com>
2022-03-06 15:49:52 +0000

    Create tpl.php

---
+++ admin/tpl.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+    ob_start();
+    if(isset($_SESSION['username'])):
+        include "init.php";
+        
+        echo "<a href='logout.php'>sniper</a>";
+        
+        include $tpl . "footer.php";
+    else:
+        header("Location: index.php");
+    endif;
+    ob_end_flush();
+?>
