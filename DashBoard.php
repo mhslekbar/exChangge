36948a1d2609fc31a04b89810242c859beb5196c
mhslekbar <mhs.lekbar@gmail.com>
2022-03-06 15:50:16 +0000

    Create DashBoard.php

---
+++ DashBoard.php
@@ -0,0 +1,30 @@
+<?php
+    session_start();
+    ob_start();
+    if(isset($_SESSION['caissier'])):
+        $pageTitle = "Table De Board";
+        include "init.php";
+        $Dashborad = new DashBoard();
+        $getBrnch = $Dashborad->getBrnch($_SESSION['userid']);
+        $today = date("Y-m-d");
+        
+        echo "<div class='container container-500 dashboard'>";
+            echo "<h1 class='text-center mt-3 mb-3'>Table de Board</h1>";
+    ?>   
+        <div class="row">
+        
+            <?=AreaDisplayLayout("transactions.php","TRANSACTIONS",$Dashborad->countTrans($getBrnch['bbid'],$today),"BRIGHTYARROW","fas fa-exchange-alt fa-5x");?>    
+            <!-- Count the customers has same town of this branch  -->
+            <?=AreaDisplayLayout("customers.php","CLIENT",$Dashborad->countCustomers($getBrnch['bbid']),"LIGHTPURPLE","fas fa-user-tie fa-5x");?>            
+            <?=AreaDisplayLayout("transfere.php","Transfere d'argent",$Dashborad->countTransferNoCust($getBrnch['bbid'],$getBrnch['bbid'],$today),"GREESEAN","fas fa-comments-dollar fa-5x");?>            
+            
+
+        </div>
+    <?php    
+        echo "</div>";
+        include $tpl . "footer.php";
+    else:
+        header("Location: index.php");
+    endif;
+    ob_end_flush();
+?>
