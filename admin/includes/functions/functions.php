0e0e75944162a1c05b3636a42938e2b0f149f3ed
mhslekbar <mhs.lekbar@gmail.com>
2022-03-06 14:04:21 +0000

    Create functions.php

---
+++ admin/includes/functions/functions.php
@@ -0,0 +1,41 @@
+<?php
+    spl_autoload_register('myAutoLoader');
+
+    function myAutoLoader ($classeName) {
+        $path = "classes/";
+        $extension = ".class.php";
+        $fileName = $path . $classeName . $extension;
+        
+        if(!file_exists($fileName)) {
+            return false;
+        }
+
+        require $fileName;
+    }   
+
+    function getTitle() {
+        global $pageTitle;
+        if(isset($pageTitle)):
+            return $pageTitle;
+        else:
+            return "Default";
+        endif;
+
+    }
+
+    function removeComma($num) {
+        return ($num - (int)$num) == 0 ? (int)$num : $num;  
+    }
+
+    function AreaDisplayLayout($link,$name,$count,$color,$font) {
+        return "<div class='col-sm-12 col-md-6 col-lg-4'>
+                    <div class='area $color mb-2'>
+                        <a href='$link'>
+                        <i class='$font'></i>
+                            <div class='content'>
+                            <strong>{$name}</strong><p class='text-center'>{$count}</p>
+                            </div>
+                        </a>    
+                    </div>
+                </div>";
+    }
