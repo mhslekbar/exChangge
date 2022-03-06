35c4d5ca6beef48b5d31d8f741460ee03ac14bd5
mhslekbar <mhs.lekbar@gmail.com>
2022-03-06 15:49:11 +0000

    Create init.mvc.php

---
+++ admin/MVC/init.mvc.php
@@ -0,0 +1,24 @@
+<?php
+
+    // Models
+    require "models/database.class.php";
+    require "models/users.class.php";
+    require "models/rates.class.php";
+    require "models/suppliers.class.php";
+    require "models/branchs.class.php";
+    require "models/chargerBranch.class.php";
+    require "models/DashBoard.class.php";
+    require "models/Customers.class.php";
+    require "models/RateBranchs.class.php";
+    require "models/BenefStats.class.php";
+
+    
+    require "../MVC/models/Transactions.class.php";
+
+    
+    
+    // Views 
+    require "views/users.view.php";
+    
+
+?>
