e9fc9d92322b46d5b34ccb8983e9391f16cf056e
mhslekbar <mhs.lekbar@gmail.com>
2022-03-06 15:49:38 +0000

    Create users.view.php

---
+++ admin/MVC/views/users.view.php
@@ -0,0 +1,17 @@
+<?php
+
+class UsersView extends Users {
+
+    // public function checkUser($usern,$pass) {
+    //     if($this->checkConn($usern,$pass) > 0) {
+    //         return $this->checkConn($usern,$pass);
+    //     }
+    // }
+
+    // public function display() {
+    //     print_r($this->getUsers());
+    // }
+}
+
+
+?>
