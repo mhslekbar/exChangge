baaee564e035427bbc18cfc82e807c3f8b2deebe
mhslekbar <mhs.lekbar@gmail.com>
2022-03-06 15:53:01 +0000

    Create init.mvc.php

---
+++ MVC/init.mvc.php
@@ -0,0 +1,16 @@
+<?php
+
+    // Models
+    require "admin/MVC/models/database.class.php";
+    require "models/DashBoard.class.php";
+    require "models/Users.class.php";
+    require "models/Transactions.class.php";
+    require "models/Customers.class.php";
+    require "models/transfere.class.php";
+    
+    
+
+    
+    
+
+?>
