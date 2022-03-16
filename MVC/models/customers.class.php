ad05b5f2243123a96d49ef668918eca2c0d2ce6b
mhslekbar <mhs.lekbar@gmail.com>
2022-03-16 16:17:28 +0000

    Update customers.class.php

--- MVC/models/customers.class.php
+++ MVC/models/customers.class.php
@@ -32,15 +32,15 @@ class Customers extends Database {
         return $stmt->rowCount();
     }
 
-    public function insertTransCust($idCust,$phone,$fname,$montant,$type) {
-        $stmt = $this->connect()->prepare("INSERT INTO zztranscustomer (tcIdCust,tcPhone,tcFullName,tcAmount,tcType,tcDate) VALUES (?,?,?,?,?,now()) ");
-        $stmt->execute([$idCust,$phone,$fname,$montant,$type]);
+    public function insertTransCust($idCust,$brnch,$phone,$fname,$montant,$type) {
+        $stmt = $this->connect()->prepare("INSERT INTO zztranscustomer (tcIdCust,tcIdBranch,tcPhone,tcFullName,tcAmount,tcType,tcDate) VALUES (?,?,?,?,?,?,now()) ");
+        $stmt->execute([$idCust,$brnch,$phone,$fname,$montant,$type]);
         return $stmt->rowCount();
     }
 
-    public function getTransCust($today) {
-        $stmt = $this->connect()->prepare("SELECT * FROM zztranscustomer tc JOIN zzcustomers c ON tc.tcIdCust = c.ccID WHERE substr(tcDate,'1','10') = ? ORDER BY tcID  DESC");
-        $stmt->execute([$today]);
+    public function getTransCust($today,$brnch) {
+        $stmt = $this->connect()->prepare("SELECT * FROM zztranscustomer tc JOIN zzcustomers c ON tc.tcIdCust = c.ccID WHERE substr(tcDate,'1','10') = ? AND tcIdBranch = ? ORDER BY tcID  DESC");
+        $stmt->execute([$today,$brnch]);
         return $stmt->fetchAll();
     }
     
@@ -63,7 +63,71 @@ class Customers extends Database {
         return $stmt->rowCount();
     }
 
+  
+    /**   START Customers No Approve  */
     
+    public function getBranchs($bbid) {
+        $stmt = $this->connect()->prepare("SELECT * FROM branchs WHERE bbid <> ?");
+        $stmt->execute([$bbid]);
+        return $stmt->fetchAll();
+    }
+    
+    public function getSymbol($idRR) {
+        $stmt = $this->connect()->prepare("SELECT * FROM rates WHERE idRR = ?");
+        $stmt->execute([$idRR]);
+        return $stmt->fetch();
+    }
+  
+    public function insertRecord($branchSender, $contactSender, $branchReceipt, $contactReceipt, $nameReceipt, $amountSender, $amountReceipt, $Benef, $type) {
+        $stmt = $this->connect()->prepare("INSERT INTO zznocustomers (nnBranchSender, nnSenderContact, nnBranchReceipt, nnReceiptContact, nnReceiptName, nnAmountSend, nnAmountReceipt, nnBenef, nnType, nnValider, nnDate) VALUES (?,?,?,?,?,?,?,?,?,0,now())  ");
+        $stmt->execute([$branchSender, $contactSender, $branchReceipt, $contactReceipt, $nameReceipt, $amountSender, $amountReceipt, $Benef, $type]);
+        return $stmt->rowCount();
+    }
+
+    //  Get Sended Transaction
+
+    public function getTransNoCustomers($branchSender,$today) {
+        $stmt = $this->connect()->prepare("SELECT DISTINCT(nnBranchReceipt), n.*,b.* FROM zznocustomers n JOIN branchs b ON nnBranchSender = bbid AND nnBranchReceipt <> bbid WHERE (nnBranchSender = ?) AND (substr(nnDate,'1','10') = ? OR nnValider = 0) ORDER BY nnID DESC");
+        $stmt->execute([$branchSender,$today]);
+        return $stmt->fetchAll();
+    }
+
+    // Get Receipt Transaction
+
+    public function getTransNoCustomersReceipt($branchReceipt,$today) {
+        $stmt = $this->connect()->prepare("SELECT DISTINCT(nnBranchSender), n.*,b.* FROM zznocustomers n JOIN branchs b ON nnBranchSender <> bbid AND nnBranchReceipt = bbid WHERE (nnBranchReceipt = ?) AND (substr(nnDate,'1','10') = ? OR nnValider = 0) ORDER BY nnID DESC");
+        $stmt->execute([$branchReceipt,$today]);
+        return $stmt->fetchAll();
+    }
+    
+
+    public function branch($bbid) {
+        $stmt = $this->connect()->prepare("SELECT * FROM branchs WHERE bbid = ?");
+        $stmt->execute([$bbid]);
+        return $stmt->fetch();
+    }
+
+    public function getBranchWHereName($bname) {
+        $stmt = $this->connect()->prepare("SELECT * FROM branchs WHERE bbBrancheName = ?");
+        $stmt->execute([$bname]);
+        return $stmt->fetch();
+    }
+
+    public function getNoCust($contactReceipt) {
+        $stmt = $this->connect()->prepare("SELECT * FROM zznocustomers WHERE nnReceiptContact = ?");
+        $stmt->execute([$contactReceipt]);
+        return $stmt->fetch();
+    }
+
+    public function updateValider($id) {
+        $stmt = $this->connect()->prepare("UPDATE zznocustomers SET nnValider = 1 WHERE nnID = ?");
+        $stmt->execute([$id]);
+        return $stmt->rowCount();
+    }  
+
+    
+    /**   END Customers No Approve  */
+
 
 }
 
